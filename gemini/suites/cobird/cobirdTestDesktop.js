e30a04dfaf97491d0c5f22920a22fa0207952ba3
Jon Lee <jdukelee@gmail.com>
2018-04-26 09:48:09 -0700

    Update cobird config and test files

--- gemini/suites/cobird/cobirdTestDesktop.js
+++ gemini/suites/cobird/cobirdTestDesktop.js
@@ -5,9 +5,27 @@ var runnerLib = currDir + '/lib/test-functions';
 var fullPageTestRunner = require(runnerLib + '/fullPageTestFunctions');
 var util = require(currDir + '/lib/util/utility-helpers');
 
+// Cobird specific element selectors.
+var dialogCloseButton = 'button.dialog__close-btn';
+var header = 'section.site-header';
+// Cobird specific functions.
+function closeSignUpDialog(actions) {
+    actions.click(dialogCloseButton);
+    actions.wait(2000);
+}
+
+function hideHeader(actions) {
+    actions.executeJS(function(window) {
+        let header = window.document.querySelector('body header:nth-child(1)');
+        header.style.visibility = 'hidden';
+    });
+    actions.wait(1000);
+}
+
 var ignoreElements = [];
 var beforeFns = [util.hideHeaderElement];
-var captureFns = [];
+var captureFnsInitialLoad = [closeSignUpDialog];
+var captureFnsAll = [];
 
 var pages = [
     '/'
@@ -15,5 +33,12 @@ var pages = [
 
 pages.forEach((page) => {
     let suiteName = (page.slice(1).length === 0 ? 'index' : page.slice(1));
-    fullPageTestRunner.runFullPageScreenshot(suiteName, page, ignoreElements, beforeFns, captureFns);
+    if (page.slice(1).length === 0) {
+        suiteName = 'index';
+        captureFns = captureFnsInitialLoad;
+    } else {
+        suiteName = page.slice(1);
+        captureFns = captureFnsAll;
+    }
+    fullPageTestRunner.runFullPageScreenshot('cobird-' + suiteName, page, ignoreElements, beforeFns, captureFns);
 });
